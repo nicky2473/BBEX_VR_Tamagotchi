23ec3d453fdb8820e8b01d94dace199d7f6ce51b
Junho-Cho <silver2473@naver.com>
2019-09-18 17:48:41 +0900

    1. 이제 닭 오브젝트가 처음에 랜덤한 장소에서 나타납니다. (문 앞/의자 위/책상 위) (The chicken object appear randomly. (In front of door/On the chair/On the desk) 2. "Talk" 버튼을 누르면 닭 오브젝트가 자신의 위치를 음성으로 말해줍니다. (When click "Talk" button, the chicken object speak where she is. (Korean))

--- src/components/Info/index.js
+++ src/components/Info/index.js
@@ -1,31 +1,26 @@
-import PropTypes from 'prop-types';
-import React, { PureComponent } from 'react';
-import InfoView from './InfoView';
-import { rotate } from '../../rotate';
+import PropTypes from "prop-types";
+import React, { PureComponent } from "react";
+import InfoView from "./InfoView";
+import { talk } from "../../talk";
 
 export default class Info extends PureComponent {
   static propTypes = {
     name: PropTypes.string.isRequired,
+    pos: PropTypes.number.isRequired
   };
 
   state = {
-    open: false,
+    open: false
   };
 
   render() {
-    const { name } = this.props
-    const { open } = this.state;
-    return (
-      <InfoView
-        name={name}
-        open={open}
-        onClick={this.handleClick}
-      />
-    );
+    const { name } = this.props;
+    return <InfoView name={name} onClick={this.handleClick} />;
   }
 
   handleClick = () => {
+    const { pos } = this.props;
     this.setState(({ open }) => ({ open: !open }));
-    rotate();
+    talk({ pos });
   };
 }
